2b98ed75167b5bf928ad04d7d9117ee1430aaa00
adi <adibush01@gmail.com>
2024-10-09 01:26:27 +0300

    solution

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -3,15 +3,15 @@
 #Creating an SSH key on server 1-->ssh-keygen -t rsa   .1
 
 #Take this key and coping him on server 2    .2
-cat ~/.ssh/id_rsa.pub (the key of server1)
-nano ~/.ssh/authorized_keys
+#cat ~/.ssh/id_rsa.pub (the key of server1)
+#nano ~/.ssh/authorized_keys
 
-3. Gives permissions to the authorized_keys file and to .ssh
-chmod 600 ~/.ssh/authorized_keys
-chmod 700 ~/.ssh
+3#. Gives permissions to the authorized_keys file and to .ssh
+#chmod 600 ~/.ssh/authorized_keys
+#chmod 700 ~/.ssh
 
-4. Go to sudo nano /etc/hosts and associate the IP address with the server2 name
-add the line --> 192.168.60.11 server2
+4#. Go to sudo nano /etc/hosts and associate the IP address with the server2 name
+#add the line --> 192.168.60.11 server2
 
-* Go to virtualbox and make sure both servers are running on the same network
-* Note that the solution of exercise 5 is solved in this exercise
+#* Go to virtualbox and make sure both servers are running on the same network
+#* Note that the solution of exercise 5 is solved in this exercise
--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+#The exercise is solved as part of the solution of exercise 4
