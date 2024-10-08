a92c1b51676d460392d3daac7b77a4e448d43f04
adi <adibush01@gmail.com>
2024-10-09 01:30:37 +0300

    solution

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -2,15 +2,15 @@
 #add fix to exercise4-server1 here
 #Creating an SSH key on server 1-->ssh-keygen -t rsa   .1
 
-#Take this key and coping him on server 2    .2
+#Take this key and coping him on server 2   .2
 #cat ~/.ssh/id_rsa.pub (the key of server1)
 #nano ~/.ssh/authorized_keys
 
-3#. Gives permissions to the authorized_keys file and to .ssh
+3#.   Gives permissions to the authorized_keys file and to .ssh
 #chmod 600 ~/.ssh/authorized_keys
 #chmod 700 ~/.ssh
 
-4#. Go to sudo nano /etc/hosts and associate the IP address with the server2 name
+4#.   Go to sudo nano /etc/hosts and associate the IP address with the server2 name
 #add the line --> 192.168.60.11 server2
 
 #* Go to virtualbox and make sure both servers are running on the same network
--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -2,15 +2,15 @@
 #add fix to exercise4-server2 here
 Creating an SSH key on server 2-->ssh-keygen -t rsa   .1#
 
-#Take this key and coping him on server 1    .2
+#Take this key and coping him on server     .2
 #cat ~/.ssh/id_rsa.pub (the key of server2)
 #nano ~/.ssh/authorized_keys
 
-3#. Gives permissions to the authorized_keys file and to .ssh
+3#.   Gives permissions to the authorized_keys file and to .ssh
 #chmod 600 ~/.ssh/authorized_keys
 #chmod 700 ~/.ssh
 
-4#. Go to sudo nano /etc/hosts and associate the IP address with the server2 name
+4#.   Go to sudo nano /etc/hosts and associate the IP address with the server2 name
 #add the line --> 192.168.60.10 server1
 
 #* Go to virtualbox and make sure both servers are running on the same network
